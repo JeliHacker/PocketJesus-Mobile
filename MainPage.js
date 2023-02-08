ceec6c168dec632ff1c742d448982745b318545d
JeliHacker <jeligooch@gmail.com>
2023-02-08 13:14:59 -0500

    add random quote button

--- MainPage.js
+++ MainPage.js
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Button } from 'react-native';
 import quotes from './data/quotes.json';
 
-
-
 function MainPage() {
-
     const [data, setData] = useState([]);
-    const [firstItem, setFirstItem] = useState([]);
+    const [currentQuote, setCurrentQuote] = useState({});
 
     useEffect(() => {
         setData(quotes);
-        setFirstItem(quotes[0])
-    }, [])
+        const randomIndex = Math.floor(Math.random() * data.length);
+        setCurrentQuote(data[randomIndex]);
+    }, []);
+
+    const getRandomQuote = () => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        setCurrentQuote(data[randomIndex]);
+    };
 
     return (
         <View style={styles.container}>
-            <ImageBackground source={require("./assets/background1.png")} resizeMode="cover" style={styles.image}>
-                <Text>Index: {firstItem.Index}</Text>
-                <Text>Verse: {firstItem.Verse}</Text>
-                <Text>Text: {firstItem.Text}</Text>
+            <ImageBackground
+                source={require('./assets/background1.png')}
+                resizeMode="cover"
+                style={styles.image}
+            >
+                <View style={styles.quoteContainer}>
+                    <Text style={styles.quote}>Verse: {currentQuote.Verse}</Text>
+                    <Text>{currentQuote.Text}</Text>
+                </View>
+                <Button
+                    onPress={getRandomQuote}
+                    title="Next"
+                    accessibilityLabel="Press this button to get a random quote"
+                />
             </ImageBackground>
         </View>
     );
@@ -27,20 +40,22 @@ function MainPage() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    },
-    background: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        resizeMode: 'contain'
+        flex: 1,
     },
     image: {
         flex: 1,
         justifyContent: 'center',
-    }
-})
+    },
+    quoteContainer: {
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        alignItems: 'center',
+    },
+    quote: {
+        alignContent: 'center',
+        padding: 10,
+        borderRadius: 10,
+        fontSize: 20,
+    },
+});
 
 export default MainPage;
