01e3429280901dc7eddc30e3aaf191ad85efe6e8
JeliHacker <jeligooch@gmail.com>
2023-02-07 19:58:21 -0500

    get data to show up on screen, add background image

--- MainPage.js
+++ MainPage.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native';
+import quotes from './data/quotes.json';
+
+
 
 function MainPage() {
+
+    const [data, setData] = useState([]);
+    const [firstItem, setFirstItem] = useState([]);
+
+    useEffect(() => {
+        setData(quotes);
+        setFirstItem(quotes[0])
+    }, [])
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>This is the main page</Text>
+        <View style={styles.container}>
+            <ImageBackground source={require("./assets/background1.png")} resizeMode="cover" style={styles.image}>
+                <Text>Index: {firstItem.Index}</Text>
+                <Text>Verse: {firstItem.Verse}</Text>
+                <Text>Text: {firstItem.Text}</Text>
+            </ImageBackground>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        resizeMode: 'contain'
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    }
+})
 
 export default MainPage;
